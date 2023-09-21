8f7b56ef2a91ea1a458a9e9fdd733767d59f173e
weerasak <weerasak.suw@vultureprime.com>
2023-09-21 10:12:19 +0000

    update with refactor

--- tx.py
+++ tx.py
@@ -29,59 +29,35 @@ def run(fromBlock,toBlock,idx):
     return cleaned_data 
 
 def batch_job(start_block,end_block):
-    schema = pa.schema([
-        ('blockNumber', pa.int32()),
-        ('index', pa.int32()),
-        ('address', pa.string()),
-        ('data', pa.string()),
-        ('blockHash', pa.string()),
-        ('topics', pa.list_(pa.string())),
-        ('transactionIndex', pa.int32()),
-        ('removed', pa.bool_()),
-        ('partitionIdx', pa.int32())
-    ])
     start_block = start_block
     end_block = end_block
+    index_size = 10000
     step = 1000
     current_block = start_block
     result = []
     while current_block <= end_block:
         start = current_block
         end = current_block + step
-        idx = start // 10000
-        if end // 10000 > start // 10000:
-            end = ((end // 10000)*10000) - 1
+        idx = start // index_size
+        if end // index_size > start // index_size:
+            end = ((end // index_size)*index_size) - 1
             result = result + run(start,end,idx)
-            df = pd.json_normalize(result)
-            df.to_parquet(s3_bucket , compression='snappy', index=False, partition_cols='partitionIdx', schema=schema)
+            save_to_s3(result,s3_bucket)
             result = []
         elif end >= end_block:
             end = end_block
             result = result + run(start,end,idx)
-            df = pd.json_normalize(result)
-            df.to_parquet(s3_bucket , compression='snappy', index=False, partition_cols='partitionIdx' ,schema=schema)
+            save_to_s3(result,s3_bucket)
         else :
             result = result + run(start,end,idx)
         current_block = end + 1
-    df = pd.json_normalize(result)
-    df.to_parquet(s3_bucket , compression='snappy', index=False, partition_cols=[ 'partitionIdx','blockNumber'])
+    save_to_s3(result,s3_bucket)
 
-def near_realime():
-    start_block = 109840567
+def near_realime(start_block):
+    start_block = start_block
     result = []
     idx_size = 1000
-    step_size = 500
-    schema = pa.schema([
-            ('blockNumber', pa.int32()),
-            ('index', pa.int32()),
-            ('address', pa.string()),
-            ('data', pa.string()),
-            ('blockHash', pa.string()),
-            ('topics', pa.list_(pa.string())),
-            ('transactionIndex', pa.int32()),
-            ('removed', pa.bool_()),
-            ('partitionIdx', pa.int32())
-        ])    
+    step_size = 500    
     while True:
         end_block = web3.eth.get_block('latest')['number']
         idx = start_block // idx_size
@@ -93,8 +69,7 @@ def near_realime():
             end_block = ((end_block // idx_size)*idx_size) - 1
             print(start_block,end_block , "AND Save")
             result = result + run(start_block,end_block,idx)
-            df = pd.json_normalize(result)
-            df.to_parquet(s3_bucket , compression='snappy', index=False, partition_cols='partitionIdx' ,schema=schema)
+            save_to_s3(result,s3_bucket)
             result = []
         else:
             print(start_block,end_block)
@@ -105,10 +80,26 @@ def near_realime():
 def get_latest_block():
     latest = web3.eth.get_block('latest')['number']
     print(latest)
+    
+def save_to_s3(data,bucket):
+    schema = pa.schema([
+            ('blockNumber', pa.int32()),
+            ('index', pa.int32()),
+            ('address', pa.string()),
+            ('data', pa.string()),
+            ('blockHash', pa.string()),
+            ('topics', pa.list_(pa.string())),
+            ('transactionIndex', pa.int32()),
+            ('removed', pa.bool_()),
+            ('partitionIdx', pa.int32())
+        ])
+    df = pd.json_normalize(data)
+    df.to_parquet(bucket , compression='snappy', index=False, partition_cols='partitionIdx', schema=schema)
+
 if web3.is_connected():
     s3_bucket = 's3://vultureprime-tx-engine/'
     # get_latest_block()
     # batch_job(0,1200)
-    near_realime()
+    near_realime(109840567)
     # a= run(20000,20000,2)
     # print(a)
